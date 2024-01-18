0f15121633f62ca2542509ce4ee34842b187f18f
Steven Gouws <steven@stevengouws.com>
2024-01-18 11:06:55 +0000

    Handle random number api errors

--- app/services/randomNumberService.js
+++ app/services/randomNumberService.js
@@ -1,5 +1,6 @@
 class RandomNumberService {
   async getRandomNumber() {
+    const rateLimitErrorCode = "5";
     try {
       const response = await fetch(
         "https://csrng.net/csrng/csrng.php?min=0&max=100"
@@ -8,6 +9,13 @@ class RandomNumberService {
         throw new Error(`HTTP error status: ${response.status}`);
       }
       const [result] = await response.json();
+      if (result.code === rateLimitErrorCode) {
+        // no need to log error as we expect to hit the rate limit from time to time
+        return undefined;
+      }
+      if (result.status !== "success") {
+        throw new Error(`API error: ${result.reason}`);
+      }
       return result.random;
     } catch (error) {
       console.log("error", error);
--- test/services/randomNumberService.test.js
+++ test/services/randomNumberService.test.js
@@ -5,18 +5,19 @@ import { randomNumberService } from "../../app/services/randomNumberService.js";
 
 describe("getRandomNumber", () => {
   describe("given that the request to fetch a number succeeds", () => {
-    const number = 50;
+    const expectedNumber = 50;
     beforeEach(async () => {
-      sinon
-        .stub(global, "fetch")
-        .resolves({ ok: true, json: () => [{ random: number }] });
+      sinon.stub(global, "fetch").resolves({
+        ok: true,
+        json: () => [{ status: "success", random: expectedNumber }],
+      });
     });
     afterEach(async () => {
       sinon.restore();
     });
     it("should return a number", async () => {
       const result = await randomNumberService.getRandomNumber();
-      expect(result).to.equal(number);
+      expect(result).to.equal(expectedNumber);
     });
   });
   describe("given that the request to fetch a number fails", () => {
@@ -34,6 +35,21 @@ describe("getRandomNumber", () => {
   describe("given that the request has an HTTP error", () => {
     beforeEach(async () => {
       sinon.stub(global, "fetch").resolves({ ok: false, status: 500 });
+    });
+    afterEach(async () => {
+      sinon.restore();
+    });
+    it("should return undefined", async () => {
+      const result = await randomNumberService.getRandomNumber();
+      expect(result).to.be.undefined;
+    });
+  });
+  describe("given that the response result does not have a status of 'success'", () => {
+    beforeEach(async () => {
+      sinon.stub(global, "fetch").resolves({
+        ok: true,
+        json: () => [{ status: "error" }],
+      });
     });
     afterEach(async () => {
       sinon.restore();
